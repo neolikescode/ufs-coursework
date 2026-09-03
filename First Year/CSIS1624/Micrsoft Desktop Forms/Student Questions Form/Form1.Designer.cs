d29f46a1aacb624688f1eb0bdf5a0f8144da2731
neolikescode <kgatlaneonel@gmail.com>
2026-09-03 16:00:10 +0200

    Student Question Forms
    This form asks the student what question they have. Saves that question.
    
    2. It then displays all the questions asked
    
    I'm just doing last minute rounds before my semester test 1, at 5pm

---
+++ First Year/CSIS1624/Micrsoft Desktop Forms/Student Questions Form/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Student_Questions_Form
+{
+    partial class frmCSIQuestionBlog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.txtQuestion = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuestion.Location = new System.Drawing.Point(25, 44);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(423, 50);
+            this.lblQuestion.TabIndex = 0;
+            this.lblQuestion.Text = "Type your questions here.";
+            // 
+            // txtQuestion
+            // 
+            this.txtQuestion.Location = new System.Drawing.Point(25, 85);
+            this.txtQuestion.Multiline = true;
+            this.txtQuestion.Name = "txtQuestion";
+            this.txtQuestion.Size = new System.Drawing.Size(538, 187);
+            this.txtQuestion.TabIndex = 1;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(25, 305);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(123, 49);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Submit";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(236, 305);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(121, 49);
+            this.btnRead.TabIndex = 3;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(447, 305);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(116, 49);
+            this.btnClose.TabIndex = 4;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // frmCSIQuestionBlog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(614, 380);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtQuestion);
+            this.Controls.Add(this.lblQuestion);
+            this.Name = "frmCSIQuestionBlog";
+            this.Text = "Student Question Blog";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.TextBox txtQuestion;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ First Year/CSIS1624/Micrsoft Desktop Forms/Student Questions Form/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Student_Questions_Form
+{
+    public partial class frmCSIQuestionBlog : Form
+    {
+        string filePath = @"Questions.txt";
+        public frmCSIQuestionBlog()
+        {
+            InitializeComponent();
+            btnSave.Select();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(filePath))
+            {
+                string question = txtQuestion.Text;
+                if (!string.IsNullOrWhiteSpace(question))
+                {
+                    using (StreamWriter w = new StreamWriter(filePath, true))
+                    {
+                        w.WriteLine(question);
+                    }
+                    txtQuestion.Clear();
+                }
+                else
+                {
+                    MessageBox.Show("Your textbox is empty!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+           
+
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            string text = File.ReadAllText(filePath);
+
+            if (!string.IsNullOrWhiteSpace(text))
+            {
+                txtQuestion.Text = text;
+            }
+            else
+            {
+                MessageBox.Show("No Questions asked!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+           
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            DialogResult answer = MessageBox.Show("Are you sure you want to close this form? ", "Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if(answer == DialogResult.Yes)
+            {
+                this.Close();
+            }
+            
+        }
+    }
+}
