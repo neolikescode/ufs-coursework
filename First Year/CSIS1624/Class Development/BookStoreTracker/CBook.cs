51103a0110ff2d54b8e201c8079d4d61ccd551b0
neolikescode <kgatlaneonel@gmail.com>
2026-09-10 16:37:28 +0200

    Class Development
    I played around with implementing classes, pretty cool concept
    
    1this program
    1. Lets choose the number of books you want to store/check
    
    2. Gets the info for those books
    
    3. Displays that info

---
+++ First Year/CSIS1624/Class Development/BookStoreTracker/CBook.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreTracker
+{
+    class CBook
+    {
+        private string sTitle;
+        private int iPages;
+        private decimal dPrice;
+
+        public string Title
+        {
+            get { return sTitle; }
+            set { sTitle = value; }
+        }
+        
+        public int Pages
+        {
+            get { return iPages; }
+            set { iPages = value; }
+        }
+        
+        public decimal Price
+        {
+            get { return dPrice; }
+            set { dPrice = value; }
+        }
+
+        public CBook()
+        {
+
+        }
+
+        public void DisplayBookInfo()
+        {
+            string sInfo = $"Book Information:\nTitle: {Title} \n" +
+                           $"Number of Pages: {Pages} \nPrice: {Price:C2} ";
+
+            Console.WriteLine(sInfo);
+        }
+
+    }
+}
---
+++ First Year/CSIS1624/Class Development/BookStoreTracker/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many books do you want to keep track of? ");
+            string userResponse = Console.ReadLine();
+
+            if(!string.IsNullOrWhiteSpace(userResponse) && int.TryParse(userResponse, out int arraySize) && arraySize > 0)
+            {
+                CBook[] books = new CBook[arraySize];
+                
+                decimal dTotal = 0;
+
+                for (int i = 0; i < books.Length; i++)
+                {
+                    Console.Clear();
+                    books[i] = new CBook(); 
+
+                    Console.Write("Enter book title: ");
+                    string sTitle = Console.ReadLine();
+                    books[i].Title = sTitle;
+
+                    Console.Write("Enter number of pages of the book: ");
+                    int iPage = int.Parse(Console.ReadLine());
+                    books[i].Pages = iPage;
+
+                    Console.Write("Enter book price: ");
+                    decimal dPrice = decimal.Parse(Console.ReadLine());
+                    books[i].Price = dPrice;
+                    dTotal += dPrice;
+                }
+                decimal dAverage = dTotal/books.Length;
+
+                foreach(CBook bookStored in books)
+                {
+                    bookStored.DisplayBookInfo();
+                    
+                }
+               Console.WriteLine($" The total average for the books is {dAverage:C2}\n");
+
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey(true);
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid number. ");
+            }
+        }
+    }
+}
