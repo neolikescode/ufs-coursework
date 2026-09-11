f5ca3f06e762c2aa17857e359e2161d531d5ab76
neolikescode <kgatlaneonel@gmail.com>
2026-09-11 23:57:50 +0200

    More Class development
    this is similar to the previous project

---
+++ First Year/CSIS1624/Class Development/MovieCollectionTracker/CMovie.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieCollectionTracker
+{
+    class CMovie
+    {
+        private string sTitle;
+        private int iDuration;
+        private decimal dPrice;
+
+       
+        public string Title
+        {
+            get { return sTitle; }
+            set { sTitle = value; }
+        }
+        
+        public int Duration
+        {
+            get { return iDuration; }
+            set { iDuration = value; }
+        }
+        
+        public decimal RentalPrice
+        {
+            get { return dPrice; }
+            set { dPrice = value; }
+        }
+
+        public CMovie()
+        {
+
+        }
+
+        public string DisplayMovieInfo()
+        {
+            return $"Movie Information\n\nTitle: {Title}\nDuration: {Duration}\nRental Price: {RentalPrice:C2}";
+        }
+
+    }
+}
---
+++ First Year/CSIS1624/Class Development/MovieCollectionTracker/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieCollectionTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many movies do you want to keep track of? ");
+
+            CMovie[] movies = new CMovie[int.Parse(Console.ReadLine())];
+
+            for(int i = 0; i < movies.Length; i++)
+            {
+                Console.Clear();
+                movies[i] = new CMovie();
+
+                Console.Write("Enter movie title: ");
+                string sMovie = Console.ReadLine();
+                movies[i].Title = sMovie;
+
+                Console.Write("Enter movie duration: ");
+                int iDuration = int.Parse(Console.ReadLine());
+                movies[i].Duration = iDuration;
+
+                Console.Write("Enter rental price: ");
+                decimal dPrice = decimal.Parse(Console.ReadLine());
+                movies[i].RentalPrice = dPrice;
+            }
+            Console.Clear();
+            decimal dTotal = 0;
+            foreach(CMovie movie in movies)
+            {
+                Console.WriteLine(movie.DisplayMovieInfo());
+                dTotal += movie.RentalPrice;
+            }
+             decimal dAverage = dTotal/movies.Length;
+            Console.WriteLine($"Average movie rental price: {dAverage:C2}");
+
+            Console.WriteLine("Press any key to exit... ");
+            Console.ReadKey(true);
+        }
+    }
+}
